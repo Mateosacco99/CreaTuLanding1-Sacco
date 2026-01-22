5af2d4053a1e3100fbd9de3e6770262ad2ac746d
Mateosacco99 <mateosacco99@gmail.com>
2026-01-21 21:32:49 -0300

    Feat: Nuevo componente BotonGenerico

---
+++ src/components/BotonGenerico.jsx
@@ -0,0 +1,28 @@
+import styles from '../styles/botonGenerico.module.css';
+
+export function BotonGenerico ({
+    tipo = 'primario',
+    children,
+    onClick,
+    disabled = false,
+    className='',
+    ...props
+}) {
+    const tiposDeBoton = [
+        'primario',
+        'secundario',
+        'danger',
+    ];
+    const tipoValido = tiposDeBoton.includes(tipo) ? tipo : 'primario';
+
+    return (
+        <button
+            className={`${styles.boton} ${styles[`boton${tipoValido.charAt(0).toUpperCase() + tipoValido.slice(1)}`]} ${className}`}
+            onClick={onClick}
+            disabled={disabled}
+            {...props}
+        >
+            {children}
+        </button>
+    );
+}
